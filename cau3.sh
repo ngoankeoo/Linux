79a15c455c6de3f449533da1de2c1c5b01822c73
ngoankeoo <ngoankeooo@gmail.com>
2020-06-28 01:43:43 -0700

    thay doi ngay het han cua tai khoan

--- cau3.sh
+++ cau3.sh
@@ -11,9 +11,16 @@ if [ $ton_tai -eq 0 ]; then
  
 else
    echo "$ten ton tai"
+   echo "-----------------------------------"
    echo "Thay doi ten dang nhap cho nguoi dung"
    echo "Nhap ten moi cua nguoi dung"
    read ten_moi
    usermod -l $ten_moi $ten
+   echo "-----------------------------------"
+   echo "Thay doi ngay het han cua tai khoan"
+   echo "Nhap ngay het han cua tai khoan dinh dang yyyy-mm-dd"
+   read ngay_het_han
+   usermod -e $ngay_het_han $ten_moi
+   
 fi
 
