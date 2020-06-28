0eff3d24c03a98f7e2a30653402521d483d4c6e5
ngoankeoo <ngoankeooo@gmail.com>
2020-06-28 01:37:17 -0700

    thay doi ten nguoi dung

--- cau3.sh
+++ cau3.sh
@@ -4,11 +4,16 @@
 echo "Nhap ten nguoi dung"
 read ten
 
+# Lenh kiem tra ton tai hay khong, bang cach kiem tra file /etc/passwd
 ton_tai=$(grep -c ^$ten: /etc/passwd)
 if [ $ton_tai -eq 0 ]; then
    echo "$ten khong ton tai"
-
+ 
 else
    echo "$ten ton tai"
-
+   echo "Thay doi ten dang nhap cho nguoi dung"
+   echo "Nhap ten moi cua nguoi dung"
+   read ten_moi
+   usermod -l $ten_moi $ten
 fi
+
